8c652c32e7a236e7b00a934bf963d444d77aa675
nvphuc2505 <nvphuc25052004@gmail.com>
2024-02-23 16:38:25 +0700

    .

--- Colour Triangle/vertexBufferObject.cpp
+++ Colour Triangle/vertexBufferObject.cpp
@@ -1,11 +1,6 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
 
 int main()
 {
